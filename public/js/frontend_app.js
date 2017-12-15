50b332517a67c23f918b9fabf9494a346ca631b8
petercz1 <email.peter.carroll@gmail.com>
2017-12-15 11:40:36 +0800

    QOTD: 	"What women and psychologists call `dropping your armor', we call 	"baring your neck."

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,1 +1,60 @@
 console.log('loaded frontend app');
+
+var frontend_app = angular.module('mean_app', []);
+frontend_app.controller('crud_controller', do_crud);
+
+function do_crud($scope, $http) {
+  console.log('inside do_crud');
+
+  $scope.read = function () {
+    console.log('reading all data');
+
+    $http.get('/api/read')
+    .then(function (results) {
+      console.log(results);
+      $scope.users = results.data;
+    });
+
+  }
+
+  $scope.read();
+
+  $scope.create = function () {
+    console.log('creating user');
+    console.log($scope.input_user);
+
+    var data = {
+      user: $scope.input_user
+    }
+
+    $http.post('/api/create', data)
+    .then(function (result) {
+      console.log(result);
+      $scope.read();
+    });
+  }
+
+  $scope.update = function (user) {
+    console.log('updating user');
+    console.log(user);
+
+    $http.put('/api/update', user)
+    .then(function (result) {
+      console.log(result);
+      $scope.read();      
+    });
+
+  }
+
+  $scope.delete = function (user) {
+    console.log('deleting user');
+    console.log(user);
+    
+    $http.delete('/api/delete/' + user._id)
+    .then(function (result) {
+      console.log(result);
+      $scope.read();            
+    });
+
+  }
+}
